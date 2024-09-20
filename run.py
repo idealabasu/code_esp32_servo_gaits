eda24c15690837112455e91dc1a5b224c3305d1a
Dan Aukes <danaukes@danaukes.com>
2024-09-19 19:57:56 -0700

    got bno055 working

--- run.py
+++ run.py
@@ -83,28 +83,32 @@ servo1_params = ServoParams()
 
 import json
 
+
+async def run_imu():
+    data = {}
+    data['temp'] = imu.temperature()
+    data['gyro'] = imu.gyro()
+    data['accel'] = imu.accel()
+    return data
+
 @app.route('/ws')
 @microdot.websocket.with_websocket
 async def read_sensor(request, ws):
     while True:
-        # time.sleep(1)
         try:
             data = await ws.receive()
+            await ws.send(data)
             print(data)
-            # await ws.send(str(time.time_ns()))
-            
-
-            # print('test')
         except Exception as e:
             print(e)
 
 @app.route('/sensordata')
 @microdot.websocket.with_websocket
 async def sensordata(request, ws):
-    for ssid in range(100):
-        data = {"sensor": str(ssid)} 
-        await ws.send(json.dumps(servo1_params.get_params()))
-        await asyncio.sleep(1)
+    while True:
+        data = await run_imu()
+        await ws.send(json.dumps(data))
+        await asyncio.sleep(.25)
 
 # Static CSS/JSS
 @app.route("/static/<path:path>")
@@ -124,60 +128,62 @@ def shutdown(request):
 
 led = Pin(2, Pin.OUT)
 
-def set_servo(value):
-    led.value(int(value))
+# def set_servo(value):
+#     led.value(int(value))
 
-    print('setting servo value: ',value)
+#     print('setting servo value: ',value)
 
-def set_frequency(value):
-    time_based_servo.f = float(value)
-    print('setting f: ',value)
+# def set_frequency(value):
+#     time_based_servo.f = float(value)
+#     print('setting f: ',value)
 
-def set_amplitude(value):
-    time_based_servo.A = float(value)
-    print('setting f: ',value)
+# def set_amplitude(value):
+#     time_based_servo.A = float(value)
+#     print('setting f: ',value)
 
-def set_offset(value):
-    time_based_servo.b = float(value)
-    print('setting f: ',value)
+# def set_offset(value):
+#     time_based_servo.b = float(value)
+#     print('setting f: ',value)
 
-def set_l0(value):
-    time_based_servo.l1 = float(value)
-    print('setting f: ',value)
+# def set_l0(value):
+#     time_based_servo.l1 = float(value)
+#     print('setting f: ',value)
 
-def set_l1(value):
-    time_based_servo.l2 = float(value)
-    print('setting f: ',value)
+# def set_l1(value):
+#     time_based_servo.l2 = float(value)
+#     print('setting f: ',value)
 
-def set_l2(value):
-    time_based_servo.l3 = float(value)
-    print('setting f: ',value)
+# def set_l2(value):
+#     time_based_servo.l3 = float(value)
+#     print('setting f: ',value)
 
-def set_l3(value):
-    time_based_servo.l4 = float(value)
-    print('setting f: ',value)
+# def set_l3(value):
+#     time_based_servo.l4 = float(value)
+#     print('setting f: ',value)
 
 
 @app.get('/')
 async def index(request):
     try:
-        set_servo((int(request.args['servoval'])))
-        set_frequency((request.args['frequency']))
-        set_amplitude((request.args['amplitude']))
-        set_offset((request.args['offset']))
-        set_l0((request.args['l0']))
-        set_l1((request.args['l1']))
-        set_l2((request.args['l2']))
-        set_l3((request.args['l3']))
+        pass
+        # set_servo((int(request.args['servoval'])))
+        # set_frequency((request.args['frequency']))
+        # set_amplitude((request.args['amplitude']))
+        # set_offset((request.args['offset']))
+        # set_l0((request.args['l0']))
+        # set_l1((request.args['l1']))
+        # set_l2((request.args['l2']))
+        # set_l3((request.args['l3']))
     except KeyError:
-        set_servo(0)
-        set_frequency(1)
-        set_amplitude(90)
-        set_offset(90)
-        set_l0(0)
-        set_l1(.25)
-        set_l2(.5)
-        set_l3(.75)
+        pass
+        # set_servo(0)
+        # set_frequency(1)
+        # set_amplitude(90)
+        # set_offset(90)
+        # set_l0(0)
+        # set_l1(.25)
+        # set_l2(.5)
+        # set_l3(.75)
     # html = template.format(led_value= led.value(),frequency=time_based_servo.f, amplitude=time_based_servo.A, offset = time_based_servo.b, l0=time_based_servo.l1, l1 = time_based_servo.l2, l2 = time_based_servo.l3, l3 = time_based_servo.l4)
     return microdot.utemplate.Template('index2.html').render()
 
@@ -193,12 +199,12 @@ def start_server():
 # servo_task = asyncio.create_task(time_based_servo.update_servo_loop())
     
 
-async def check_time():
-    ii = 0
-    while True:
-        # print(ii)
-        ii+=1
-        await asyncio.sleep(0.5)
+# async def check_time():
+#     ii = 0
+#     while True:
+#         # print(ii)
+#         ii+=1
+#         await asyncio.sleep(0.5)
 
 # time_task = asyncio.create_task(check_time())
 
--- static/mylibrary.js
+++ static/mylibrary.js
@@ -16,13 +16,13 @@ function myformsubmit(event) {
     }
 
     const message = JSON.stringify(data);
-    console.log("message: " + message);
+    console.log("sent message: " + message);
 
     ws.send(message);
     document.getElementById('amplitude').value = ''
     document.getElementById('frequency').value = ''
     // log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
-    log.textContent = JSON.stringify(data);
+    // log.textContent = JSON.stringify(data);
 }
 
 const ws = new WebSocket(`ws://${location.host}/ws`);
@@ -42,10 +42,12 @@ ws.onmessage = function (message) {
         // };
     // } else 
     {
-        console.log("message: " + message.data);
+        console.log("received message: " + message.data);
+        const obj = JSON.parse(message.data);
         // msgDiv.innerHTML = message.data;
-        document.getElementById('amplitude').value = message.data.a
-        document.getElementById('frequency').value = message.data.f
+        document.getElementById('amp1').innerHTML = obj.amplitude
+        document.getElementById('freq1').innerHTML = obj.frequency
+        // document.getElementById('frequency').value = message.data.f
     }
 }
 
@@ -56,18 +58,30 @@ ws2.binaryType = "blob";
 ws2.addEventListener("open", event => {console.log("Websocket connection opened");});
 ws2.addEventListener("close", event => {console.log("Websocket connection closed");});
 ws2.onmessage = function (message) {
-    const msgDiv = document.createElement('div');
-    msgDiv.classList.add('msgCtn');
-    if (message.data instanceof Blob) {
-        reader = new FileReader();
-        reader.onload = () => {
-            msgDiv.innerHTML = reader.result;
-            document.getElementById('messages').appendChild(msgDiv);
-        };
-    } else {
+    // const msgDiv = document.createElement('div');
+    // msgDiv.classList.add('msgCtn');
+    // if (message.data instanceof Blob) {
+        // reader = new FileReader();
+        // reader.onload = () => {
+            // msgDiv.innerHTML = reader.result;
+            // document.getElementById('messages').appendChild(msgDiv);
+        // };
+    // } else 
+    {
         console.log("message: " + message.data);
-        msgDiv.innerHTML = message.data;
-        document.getElementById('messages').appendChild(msgDiv);
+        // msgDiv.innerHTML = message.data;
+        // document.getElementById('amplitude').value = message.data.a
+        // document.getElementById('frequency').value = message.data.f
+        const obj = JSON.parse(message.data);
+
+        document.getElementById('temp1').innerHTML = obj.temp
+        document.getElementById('ax').innerHTML = obj.accel[0]
+        document.getElementById('ay').innerHTML = obj.accel[1]
+        document.getElementById('az').innerHTML = obj.accel[2]
+        document.getElementById('wx').innerHTML = obj.gyro[0]
+        document.getElementById('wy').innerHTML = obj.gyro[1]
+        document.getElementById('wz').innerHTML = obj.gyro[2]
+
     }
 }
 
